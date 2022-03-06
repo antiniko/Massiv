5cee557f5fd97d36a6b0a495bba7f44c8082ec63
anti.niko <anti.niko@ya.ru>
2022-03-07 00:07:02 +0300

    all done

---
+++ zadacha2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+int X(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("нету третьей цифры");
+        return num;
+    }
+    else if (num > 999)
+    {
+        Console.WriteLine("Слишком много цифр");
+        return num;        
+    }
+    else
+    {
+    int n1 = num / 100;
+    int n2 = (num / 10) - (n1 * 10);
+    int n3 = num - (n1 * 100 +n2 * 10);
+    return n3;
+    }
+}
+
+Console.WriteLine("Write nuber from 100 to 999");
+int number = Convert.ToInt32(Console.ReadLine());
+X(number);
+Console.Write("Third nuber is: ");
+Console.Write(X(number));
+
+//к сожалению не получается пока что сделать так, что бы выводил только одну строку.
---
+++ zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void den(int num)
+{
+    if(num < 5 )
+    {
+    Console.WriteLine("Working day");   
+    }
+    else  
+    {
+    Console.WriteLine("Weekend");
+    }
+}
+Console.WriteLine("Write nuber from 1 to 7");
+int number =
+ Convert.ToInt32(Console.ReadLine());
+den(number);
